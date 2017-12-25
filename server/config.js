09c723d03ab4addebb46609c1f3263c36cea4b9f
Worthcn <git@itsworth.cn>
2017-12-25 15:51:10 +0800

    config.js

--- server/config.js
+++ server/config.js
@@ -21,7 +21,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: '',
+        pass: 1',
         char: 'utf8mb4'
     },
 
@@ -34,7 +34,7 @@ const CONF = {
         // Bucket 名称
         fileBucket: 'qcloudtest',
         // 文件夹
-        uploadFolder: ''
+        uploadrFolder: ''
     },
 
     // 微信登录态有效期
